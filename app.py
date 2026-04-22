5c056ce3512ccf70f9b3c3a1a6d2b631778211b7
a29815776-tech <a29815776@gmail.com>
2026-04-22 18:54:31 +0800

    ask contact info after quote, fix photo forwarding with separate push calls

--- app.py
+++ app.py
@@ -288,6 +288,13 @@ def handle_message(event):
 
     if msg in ["重新", "重來", "再估一個", "開始", "報價"]:
         line_states[uid] = {}
+    elif line_states.get(uid, {}).get("waiting_contact"):
+        if msg != "不用":
+            notify_owner(f"📞 客戶留下聯絡方式：{msg}")
+        line_states[uid] = {}
+        line_bot_api.reply_message(event.reply_token,
+            TextSendMessage(text="收到！師傅會盡快與您聯絡 😊\n如需再估一個請輸入「再估一個」"))
+        return
 
     if uid not in line_states:
         line_states[uid] = {}
@@ -349,9 +356,9 @@ def handle_message(event):
     # 全部齊了，出報價
     quote = line_format_quote(state["service"], state["material"], state["ping"], state["floor"], state["area"])
     notify_owner(f"🔔 新報價通知\n項目：{state['service']}／{state['material']}\n坪數：{int(state['ping'])}坪／{state['floor']}\n區域：{state['area']}\n預估：{quote.split('💰')[1].split('元')[0].strip()} 元")
-    line_states[uid] = {}
+    line_states[uid] = {"waiting_contact": True}
     line_bot_api.reply_message(event.reply_token,
-        TextSendMessage(text=quote + "\n\n如需再估一個請輸入「再估一個」\n\n也可以上傳現場照片或平面圖，讓師傅更了解施工狀況 📷"))
+        TextSendMessage(text=quote + "\n\n也可以上傳現場照片或平面圖，讓師傅更了解施工狀況 📷\n\n請問方便留下電話或 LINE ID 嗎？師傅可主動與您聯繫（不方便可直接輸入「不用」）"))
 
 
 @app.route("/img/<img_id>")
@@ -371,10 +378,8 @@ def handle_media(event):
         img_url = f"{BASE_URL}/img/{img_id}"
         if OWNER_LINE_ID:
             try:
-                line_bot_api.push_message(OWNER_LINE_ID, [
-                    TextSendMessage(text=f"📷 客戶上傳了照片\n用戶ID：{uid}"),
-                    ImageSendMessage(original_content_url=img_url, preview_image_url=img_url)
-                ])
+                line_bot_api.push_message(OWNER_LINE_ID, TextSendMessage(text=f"📷 客戶上傳了照片"))
+                line_bot_api.push_message(OWNER_LINE_ID, ImageSendMessage(original_content_url=img_url, preview_image_url=img_url))
             except Exception:
                 pass
     else:
