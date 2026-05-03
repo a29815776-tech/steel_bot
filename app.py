ba558ca6990a743e2923d8b59c26426b2a756e98
a29815776-tech <a29815776@gmail.com>
2026-05-03 22:33:30 +0800

    show customer contact info and 專人儘快為你服務 in quote reply

--- app.py
+++ app.py
@@ -341,7 +341,7 @@ def handle_message(event):
         line_states[uid] = {}
         line_bot_api.reply_message(event.reply_token,
             TextSendMessage(
-                text=quote + "\n\n請上傳照片或手繪平面草圖可為您免費設計與建議喔\n\n如需場勘請留詳細地址電話\n\n如需直接聯繫師傅：https://line.me/ti/p/~0973687898",
+                text=quote + f"\n\n您留的聯絡資訊：{msg}\n\n專人儘快為你服務 😊\n\n請上傳照片或手繪平面草圖可為您免費設計與建議喔\n\n如需場勘請留詳細地址電話\n\n如需直接聯繫師傅：https://line.me/ti/p/~0973687898",
                 quick_reply=QuickReply(items=[
                     QuickReplyButton(action=MessageAction(label="繼續估價請按這裡", text="繼續估價請按這裡"))
                 ])
